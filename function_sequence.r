1a711eea21cbbf1844647e7a1d13e8817cfd1652
benjamin-elusers <benjamin.dubreuil@weizmann.ac.il>
2020-12-20 17:22:20 +0200

    - Create utils.r: General purpose functions organized by action/data type
    - Create function_sequence.r:
    Functions for optimal use of biological sequences

---
+++ function_sequence.r
@@ -0,0 +1,261 @@
+### Playing with SGD (Saccharomyces Genome Database) ---------------------------
+SGD.nomenclature = function(coding=T,rna=F){
+  nuclear = "[Y][A-P][LR][0-9]{3}[WC](?:-[A-Z])?"
+  mito = "Q[0-9]{4}"
+  plasmid = "R[0-9]{4}[WC]"
+  tRNA = "t[ATCG]\\([ATCG]{3}\\)[A-P][0-9]+"
+  snRNA = "snR[0-9]+[a-z]"
+  rRNA = "RDN[0-9]+\\-?[0-9]"
+
+  ORF = paste(collapse='|',sprintf("(%s)",c(nuclear,mito,plasmid)))
+  RNA = paste(collapse='|',sprintf("(%s)",c(tRNA,snRNA,rRNA)))
+  if( coding & rna ){ return(sprintf("%s|%s",ORF,RNA)) }
+  if( coding & !rna ){ return(ORF) }
+  if( !coding & rna ){ return(RNA) }
+  if( !coding & !rna ){ return(nuclear) }
+}
+
+load.sgd.features = function(){
+  require(stringr)
+  sgd_feat.url = "http://sgd-archive.yeastgenome.org/curation/chromosomal_feature/SGD_features.tab"
+  sgd.feat = read.delim2(sgd_feat.url, sep='\t', quote = "",
+                         header=F, fill=T, strip.white=T,stringsAsFactors = F,
+                         col.names = c('sgdid','type','qual','name',
+                                       'gname','alias','parent','sgdid2',
+                                       'chr','start','end','strand','gpos',
+                                       'coordv','seqv','desc')  )
+  return(sgd.feat)
+}
+
+
+load.sgd.CDS = function(withORF=T) {
+  require(stringr)
+  sgd.url = "http://sgd-archive.yeastgenome.org/sequence/S288C_reference/orf_dna/orf_coding_all.fasta.gz"
+  SGD = load.genome(sgd.url)
+  regexSGD = "(S[0-9]{9})"
+  if(withORF){ # ORF identifier
+    names(SGD) = str_extract(names(SGD), SGD.nomenclature() )
+  }else{       # SGD ID
+    names(SGD) = str_extract(names(SGD), regexSGD)
+  }
+  #names(SGD) = subname(names(SGD),sep=" ",lc=F)
+  return(SGD)
+}
+
+load.sgd.proteome = function(withORF=T) {
+  require(stringr)
+  sgd.url = "http://sgd-archive.yeastgenome.org/sequence/S288C_reference/orf_protein/orf_trans_all.fasta.gz"
+  SGD = load.proteome(sgd.url)
+  regexSGD = "(S[0-9]{9})"
+  if(withORF){ # ORF identifier
+    names(SGD) = str_extract(names(SGD), SGD.nomenclature() )
+  }else{       # SGD ID
+    names(SGD) = str_extract(names(SGD), regexSGD)
+  }
+  #names(SGD) = subname(names(SGD),sep=" ",lc=F)
+  return(SGD)
+}
+
+
+# Biological annotations (mapped to Uniprot) -----------------------------------
+get.uniprot.pmid = function(uniprot) {
+  require(AnnotationDbi)
+  require(org.Sc.sgd.db)
+  require(dplyr)
+  pmid = AnnotationDbi::select(x = org.Sc.sgd.db,
+                               columns = c('PMID'),
+                               keys = uniprot, keytype = 'UNIPROT') %>%
+    group_by(UNIPROT) %>%
+    mutate(n_pub = n_distinct(PMID), PMIDS = toString(PMID)) %>%
+    dplyr::select(-PMID) %>%
+    distinct()
+  return(pmid)
+}
+
+get.uniprot.go = function(uniprot) {
+  require(AnnotationDbi)
+  require(org.Sc.sgd.db)
+  require(dplyr)
+  require(tidyr)
+  require(GO.db)
+  go = AnnotationDbi::select(x = org.Sc.sgd.db,
+                             columns = c('GO', 'ONTOLOGY', 'EVIDENCE'),
+                             keys = uniprot, keytype = 'UNIPROT'
+  ) %>%
+    mutate(obsolete = GO %in% keys(GOOBSOLETE)) %>%
+    filter(!obsolete & !is.na(GO)) %>%
+    mutate(goterm = Term(GO), onto=ONTOLOGY) %>% arrange(UNIPROT,GO) %>%
+    group_by(UNIPROT) %>% mutate( ALL= n_distinct(GO) ) %>%
+    group_by(UNIPROT,ONTOLOGY) %>% add_count(name='ngo') %>%
+    tidyr::pivot_wider(names_from=onto, values_from=ngo, values_fn=list(ngo=sum), values_fill=list(ALL=0)) %>%
+    group_by(UNIPROT) %>% fill(MF,CC,BP,.direction = 'downup') %>%
+    group_by(GO) %>% mutate(shared=n_distinct(UNIPROT)) %>%
+    group_by(UNIPROT,GO) %>% mutate( EVI_ALL = str_c(EVIDENCE,collapse = '|'))
+
+  goevidence = go %>% mutate(checked = TRUE) %>% ungroup() %>%
+    pivot_wider( id_cols = c(UNIPROT, GO), names_from = EVIDENCE,
+                 values_from = checked, values_fn = list(checked = sum)
+    ) %>% mutate( n_evi = rowSums(dplyr::select(.,IDA:RCA),na.rm=T) )
+
+  goinfo = dplyr::select(go,-EVIDENCE) %>%
+    left_join(goevidence %>% dplyr::select(-c(IDA:RCA))) %>%
+    mutate(has_biodata = replace_na(ND<1,replace = TRUE)) %>%
+    dplyr::select(-ND)%>%
+    distinct() %>% ungroup()
+  return(goinfo)
+}
+
+get.uniprot.sgd = function(uniprot) {
+  require(AnnotationDbi)
+  require(org.Sc.sgd.db)
+  require(dplyr)
+  require(GO.db)
+
+  get.sgd_annot = function(desc, annotation = "", sep = ";") {
+    require(stringr)
+    found = str_extract_all(tolower(desc), pattern = sprintf("[^%s]*%s[^%s]+", sep, annotation, sep))
+    sapply(found, paste, collapse = ";")
+  }
+
+  parse.sgd_annot = function(desc, exclude = c()) {
+    annotations = unlist(strsplit(x = tolower(desc), split = ";"))
+    excluded = unlist(strsplit(x = paste(exclude), split = ";"))
+    paste(annotations[!annotations %in% excluded], collapse = ";")
+  }
+
+  sgdinfo = AnnotationDbi::select( x=org.Sc.sgd.db,
+                                   columns = c('ORF', 'GENENAME', 'DESCRIPTION'),
+                                   keys = uniprot, keytype = 'UNIPROT') %>%
+    distinct() %>%
+    mutate(FUNCTION = tolower(str_extract(DESCRIPTION, "[^;]+"))) %>%
+    mutate(ROLE = get.sgd_annot(DESCRIPTION, annot = "(role| act|require)", sep = ";,")) %>%
+    mutate(LOC = get.sgd_annot(DESCRIPTION, annot = "(localize[sd]|localization)"))  %>%
+    mutate(ORTHO = get.sgd_annot(DESCRIPTION, annot = "(ortholog|paralog|homolog)")) %>%
+    mutate(COMPLEX = get.sgd_annot(DESCRIPTION, annot = "(complex|subunit|oligomer|mer)"))
+
+  sgdinfo$OTHER = sapply(1:nrow(sgdinfo), function(x) {
+    parse.sgd_annot(sgdinfo$DESCRIPTION[x],
+                    exclude = sgdinfo[x, c("FUNCTION", "ROLE", "LOC", "ORTHO", "COMPLEX")])
+  })
+
+  return(sgdinfo %>% dplyr::select(-DESCRIPTION))
+}
+
+get.mapping.sgd.to.uniprot = function(tax=559292,input=id_sgd){
+  # Get mapping for uniprot identifiers not included in UniRef proteome
+  # Check that duplicated sequences are not associated to different uniprot identifiers
+  require(tictoc)
+  require(UniProt.ws)
+  uniprot  = UniProt.ws::UniProt.ws(taxId=tax) # 559292 S. cerevisiae 288C (maintained by SGD)
+  tic('Retrieving mapping between uniprot accession and SGD ID...')
+  # TAKES ~50sec for about 7000 ids
+  mapped = UniProt.ws::select(x=uniprot,
+                              keys=unique(input),  keytype = "SGD",
+                              columns = c("SGD","UNIPROTKB")) %>%
+    filter(!is.na(UNIPROTKB)) %>% distinct()
+  toc(log=T)
+  return(mapped)
+}
+
+load.sgd.to.uniprot = function(tax=559292,input_id=id_sgd){
+  if(missing(input_id)){ id_sgd = unique(load.sgd.features()[['sgdid']]) }
+  # Download uniprot information (sequence,annotations...) for mapped SGD identifiers
+  sgd_mapped = get.mapping.sgd.to.uniprot(input=input_id)
+  require(UniProt.ws)
+  uniprot  = UniProt.ws::UniProt.ws(taxId=tax) # 559292 = S. cerevisiae 288C (maintained by SGD)
+  tic('Retrieving infos (sequence,annotations...) for SGD mapped uniprot entry...')
+  # TAKES ~220sec for about 7000 ids
+  sgd2uni_info = UniProt.ws::select(
+    x=uniprot, keys=sgd_mapped$UNIPROTKB, keytype = "UNIPROTKB",
+    columns = c("SGD","UNIPROTKB","REVIEWED","EXISTENCE","SCORE","LENGTH","FAMILIES",
+                "PROTEIN-NAMES","SUBCELLULAR-LOCATIONS","COMMENTS","KEYWORDS",
+                "SEQUENCE")
+  )
+  sgd2uni_info = sgd2uni_info %>%
+    dplyr::rename(SUBLOC =`SUBCELLULAR-LOCATIONS`, PNAMES = `PROTEIN-NAMES`) %>%
+    transmute(L = as.numeric(LENGTH))
+  toc(log=T)
+  SGD2UNI = inner_join(sgd_mapped,sgd2uni_info) %>% distinct()
+  return(SGD2UNI)
+}
+
+parse.uniprot.subcellular_locations = function(subloc){
+  require(stringr)
+  require(tidyverse)
+  # subloc should be a named vector
+  # Names = identifiers
+  # Values = subcellular locations uniprot annotation
+
+  # UNIPROT SUBCELLULAR LOCATIONS contains extra information such as:
+  # Annotation always starts with "SUBCELLULAR LOCATION:"
+  # Annotation of isoforms/subunits are preceded by square brackets "[]"
+  # For isoform "SUBCELLULAR LOCATION:" is repeated within the annotation
+  # Evidence Code and PMID wrapped around curly brackets "{ECO...|PMID...}"
+  # The sentence followed by "Note=" may contain additional information about localization
+  loctag  =  "(SUBCELLULAR LOCATION: )"
+  evidence = "(\\{[^\\{\\}]+\\})"
+  isoform = "(\\[[^\\[\\]]+\\]:)"
+  note = "(Note\\=.+\\.)"
+  end_note = sprintf("(%s$)",note)
+  punc='(insoluble|aggregate|foci|punctate|granules)'
+
+  LOC = subloc  %>%
+    # ERASE NON-ESSENTIAL PARTS OF ANNOTATION (keeping terms corresponding to subcellular compartments)
+    str_replace_all(pattern=loctag,replacement="") %>%
+    str_replace_all(pattern=evidence,replacement="") %>%
+    str_replace_all(pattern=end_note,replacement="") %>%
+    str_replace_all(pattern=punc,replacement="") %>%
+    str_replace_all(pattern=isoform,replacement="") %>%
+    str_replace_all(pattern=paste0(note,"(?=[;,])"),replacement="") %>%
+    str_to_lower() %>%
+    # REMOVE SPACE IN BETWEEN SEPARATORS OF ANNOTATIONS
+    str_replace_all(pattern="(?<=[;.,])( +)|( +)(?=[;.,])",replacement="") %>%
+    # SPLIT BY TERMS (hopefully subcellular compartments only)
+    stringi::stri_split_regex(pattern="[;.,]",omit_empty = T) %>%
+    # GIVE BACK THE PROTEINID TO EACH SET OF TERMS
+    set_names(nm=names(subloc))
+
+  uni.loc = enframe(LOC,name='id',value='loc') %>% bind_cols(SUBLOC = subloc) %>%
+    unnest(cols=c(loc)) %>%
+    mutate( loc = str_replace_all( str_trim(loc,side="both"), c("-" = ".", "[ ]+" = "_")) ) %>%
+    filter(!is.na(loc)) %>%
+    distinct() %>%
+    mutate( HAS_FOCI = str_detect(string = SUBLOC, pattern = punc) ) %>%
+    mutate( HAS_ISOFORM = str_detect(string = SUBLOC, pattern = isoform) ) %>%
+    dplyr::select(-SUBLOC)
+
+  return(uni.loc)
+}
+
+get.uniprot.localization = function(annot,loc_to_columns=T){
+  if(missing(annot)){
+    message(sprintf("Requires annotation table with 2 columns:\n(1) UNIPROTKB/SGD identifier\n(2) Uniprot subcellular location annotations"))
+    stop("Use 'load.sgd.to.uniprot()' function to load annotations from uniprot for yeast.")
+  }
+  require(tidyverse)
+  require(stringr)
+  require(hablar)
+  uni.annot = annot %>%
+    # dplyr::rename_if( SUBLOC = `SUBCELLULAR-LOCATIONS` ) %>%
+    filter(one2one) %>%
+    dplyr::select(SGD,UNIPROTKB,EXISTENCE,SCORE,FAMILIES,SUBLOC) %>%
+    hablar::convert( hablar::chr(SCORE) ) %>%
+    mutate( SCORE = fct_relabel(SCORE, function(x){ gsub(" out of ","/",x) }),
+            has_LOC = !is.na(SUBLOC))
+
+  # GET UNIPROT ANNOTATION OF SUBCELLULAR LOCATIONS AS NAMED VECTOR
+  subloc = uni.annot  %>% filter(has_LOC) %>% dplyr::select(UNIPROTKB,SUBLOC) %>% deframe()
+  uni.loc = parse.uniprot.subcellular_locations(subloc)
+
+  if( loc_to_columns ){
+    uni.isloc  = uni.loc %>%  mutate(seen=1) %>%
+      pivot_wider( id_cols = c('id','HAS_FOCI','HAS_ISOFORM'),
+                   names_from = 'loc',
+                   values_from = 'seen', values_fn=list(seen = sum),values_fill = list(seen=0)) %>%
+      group_by(id,HAS_FOCI,HAS_ISOFORM)
+    return(uni.isloc)
+  }
+  return(uni.loc)
+}
+
+
---
+++ utils.r
@@ -0,0 +1,308 @@
+#colored_fruits = c('yellow','banana','red','apple','green','grape')
+
+# Testing/Subsetting -----------------------------------------------------------
+is.whole  <- function(x){ all(floor(x) == x) }      # checks if a value has decimal part (not necessarily integer e.g. 1.0 is whole)
+is.binary <- function(x){ all(1*x %in% 0:1) }       # checks if a value is 0/1 (binary/logical)
+is.even   <- function(x){ as.integer(x) %% 2 == 0 } # checks if a value is even
+is.odd    <- function(x){ as.integer(x) %% 2 != 0 } # checks if a value is odd
+is.dup    <- function(x){ x %in% x[duplicated(x)] } # detects duplicates
+mid       <- function(x){ ifelse(is.even(length(x)), length(x)/2, (length(x)+1)/2) }
+
+get.even  <- function(x){ x[is.even(x)] } # returns even values
+get.odd   <- function(x){ x[is.odd(x)]  } # returns odd values
+get.dup   <- function(x){ x[is.dup(x)]  } # returns duplicates
+get.mid   <- function(x){ x[mid(x)] }     # returns the middle value
+get.half1 <- function(x){ x[seq(1,mid(x))] }  # returns first half of vector
+get.half2 <- function(x){ x[seq(mid(x)+1,length(x))] }   # returns last half of vector
+evens     <- function(x){ x[seq(1,length(x),by=2)] } # returns values at even indexes
+odds     <- function(x){ x[seq(2,length(x),by=2)] }  # returns values at odd indexes
+
+last      <- function(x){ tail(x, n = 1) }             # returns last element of a vector
+b4.last   <- function(x){ head( tail(x,n=2) , n = 1) } # returns element before last
+but.last  <- function(x){ head(x, n = -1) }            # returns vector w/o last element
+from.last <- function(x,n=2){ head(x, n = -abs(n)) }   # returns vector w/o last element
+
+unfactor <- function(x){ as.numeric(as.character(x)) } # forces conversion of factors to numbers
+compact  <- function(x){ Filter(Negate(is.null), x) }  # removes null element in list
+
+# Counting ---------------------------------------------------------------------
+ulen   <- function(x){ return( length(unique(x)) ) } # gets length of unique values
+norm01 <- function(x){ return( scale(x,center=min(x),scale=diff(range(x)))[,1] ) } # scales values between 0 and 1
+maxrep <- function(x,n){ sort(table(x), decreasing = TRUE)[1:n] } # gets the Nth most repeated values
+sum.na <- function (x,notNA=FALSE){ sum(is.na(x) == !notNA) } # returns sum of NA or not-NA values
+rowsNA = function(m){ # counts how many rows have NAs
+  nas = apply(as.matrix(m),1,sum.na)
+  if(sum(nas)>0){ message(sprintf('%s rows contain NA values.',sum(nas))) }
+  return(nas)
+}
+
+# Strings ----------------------------------------------------------------------
+str2chr  <- function(x){ return( unlist(strsplit(x,sep='')) ) } # split string by character
+xxS      <- function(x,sx,s='.'){ paste0(x,s,sx) } # Add suffix to a string
+Pxx      <- function(x,px,s='.'){ paste0(px,s,x) } # Add prefix to a string
+starting <- function(str,pre){ str[ startsWith(str,pre) ] }
+ending   <- function(str,suf){ str[ endsWith(str,suf) ] }
+
+paste.even <- function(x,s='-'){ paste(evens(x),odds(x),sep=s) }
+paste.odd  <- function(x,s='-'){ c(paste.even(but.last(x),s), last(x)) }
+paste.pair <- function(x,s='-'){
+  if( is.even(length(x)) ){ return(paste.even(x,s)) }
+  return(paste.odd(x,s))
+}
+
+rm.dup.str = function(str,sep='.'){ # removes duplicated word in string
+  words = strsplit(str,paste0("\\",sep))
+  words.unique = lapply(words,unique)
+  new.str = unlist(lapply(words.unique,paste,collapse=sep))
+  return(new.str)
+}
+
+repchar <- function(char,times){ # replicates a character multiple times as a string
+  return(paste( rep(char, times), collapse='' ))
+}
+
+subname=function(name,sep="\\.",lc=F){ # extracts substring until first separator
+  b4sep = sprintf("([^%s]+).+",sep)
+  part1 = sub(b4sep, "\\1", x=name)
+  if(lc){ tolower(part1) }
+  return(part1)
+}
+
+strfind = function(strings, patterns){ # search multiple patterns in character vectors
+  sapply(patterns,  function(p){ grep(x = strings, pattern = p, value = T) })
+}
+
+# Sequences --------------------------------------------------------------------
+load.proteome = function(url,nostop=T) {
+  require(Biostrings)
+  p = Biostrings::readAAStringSet(filepath = url)
+  if(nostop){ # Remove the trailing star from amino acid sequence (stop codon)
+    star = Biostrings::subseq(p,start=-1) == '*'
+    p = Biostrings::subseq(p,start=1,  end=width(p)-star)
+  }
+  return(p)
+}
+
+load.genome = function(url) {
+  require(Biostrings)
+  g = Biostrings::readDNAStringSet(filepath = url)
+  return(g)
+}
+
+
+
+
+
+
+
+
+getmode <- function(v) {
+  uniqv <- unique(v)
+  uniqv[which.max(tabulate(match(v, uniqv)))]
+}
+
+
+
+trim.lead <- function (x)  sub("^\\s+", "", x) # returns string w/o leading whitespace
+trim.trail <- function (x) sub("\\s+$", "", x) # returns string w/o trailing whitespace
+trim <- function (x) gsub("^\\s+|\\s+$", "", x) # returns string w/o leading or trailing whitespace
+
+trim.NaN <- function(d=data) d[apply(d,2,function(x) any(is.nan(x))),] # returns a vector without NaN
+
+
+
+
+# returns sum and mean with/out NA value
+sum_mean <- function (x,na.rm=T,as.list=T){
+  res = list( 'n'= length(x), 'sum'=sum(x,na.rm=na.rm), 'mu' = mean(x,na.rm=na.rm), 'na' = sum(is.na(x)))
+  if( !as.list ){  return(unlist(res)) }
+  return(res)
+}
+
+
+RoundUpToNearest <- function(nb, roundto=1){ # Round up numbers to unit or decimal specified
+  if (roundto){ return(nb) }
+  else{ return( ceiling(nb / roundto) * roundto ) }
+}
+
+RoundDownToNearest <- function(nb, roundto=1){ # Round down numbers to unit or decimal specified
+  if (roundto){ return (nb) }
+  else{ return( floor(nb / roundto) * roundto ) }
+}
+
+RoundToNearest <- function(nb,roundto){
+  a = RoundDownToNearest(nb,roundto);
+  b = RoundUpToNearest(nb,roundto);
+  if( abs(nb-a) < abs(nb-b) ){ return(a) }
+  else if( abs(nb-a) > abs(nb-b) ){ return(b) }
+  else{ return(nb) }
+}
+
+# Format text ------------------------------------------------------------------
+percent <- function(x, digits = 2, format = "f", ...) {
+  if(all(x>=0) & all(x<=1)){
+    paste0(formatC(100 * x, format = format, digits = digits, ...), "%")
+  }else if(all(x>=0) & all(x<=100)){
+    paste0(formatC(x, format = format, digits = digits, ...), "%")
+  }
+}
+
+percent0 <- function(x, format = "f", ...) {
+  percent(x,digits=0,format=format)
+}
+
+toUnit <- function(x, digits = 0, format = "f", units,...) {
+  if( missing(units) ){ print("You need to specify a string for the units") }
+  paste0(formatC(x, format = format, digits = digits, ...), as.character(units) )
+}
+
+rank0 <- function(x) {
+  sapply( x, function(d){
+    d.chr = as.character(d); ndigits = nchar(d.chr);
+    last.digit = as.numeric(substr(d.chr,ndigits,ndigits))
+    twolast.digit = as.numeric(substr(d.chr,ndigits-1,ndigits))
+    if( !(last.digit %in% c(1,2,3)) | twolast.digit %in% c(11,12,13) ){ return(paste0(d,"th")) }
+    if( last.digit == 1 & twolast.digit != 11){ return(paste0(d,"st")) }
+    if( last.digit == 2 & twolast.digit != 12){ return(paste0(d,"nd")) }
+    if( last.digit == 3 & twolast.digit != 13){ return(paste0(d,"rd")) }
+  }
+  )
+}
+
+toUnits <- function(x){
+  if( x >= 1e+24 ){ return(c(pre="Y",x=format(x/1e+24,digit=1)))       }
+  else if( x >= 1e+21  &  x < 1e+24 ){ return(c(pre="Z",x=format(x/1e21,digit=1)))   }
+  else if( x >= 1e+18  &  x < 1e+21 ){ return(c(pre="E",x=format(x/1e18,digit=1)))   }
+  else if( x >= 1e+15  &  x < 1e+18 ){ return(c(pre="P",x=format(x/1e15,digit=1)))   }
+  else if( x >= 1e+12  &  x < 1e+15 ){ return(c(pre="T",x=format(x/1e12,digit=1)))   }
+  else if( x >=  1e+9  &  x < 1e+12 ){ return(c(pre="G",x=format(x/1e9,digit=1)))    }
+  else if( x >=  1e+6  &  x < 1e+9  ){ return(c(pre="M",x=format(x/1e6,digit=1)))    }
+  else if( x >=  1e+3  &  x < 1e+6  ){ return(c(pre="k",x=format(x/1e3,digit=1)))    }
+  else if( x >=  1e+2  &  x < 1e+3  ){ return(c(pre="h",x=format(x/1e2,digit=1)))    }
+  else if( x >=  1e+1  &  x < 1e+2  ){ return(c(pre="da",x=format(x/1e1,digit=1)))   }
+  else if( x <   1e+1  &  x > 1e-1  ){ return(c(pre=" ",x=format(x,digit=1)))        }
+  else if( x <=  1e-1  &  x > 1e-2  ){ return(c(pre="d",x=format(x/1e-1,digit=1)))   }
+  else if( x <=  1e-2  &  x > 1e-3  ){ return(c(pre="c",x=format(x/1e-2,digit=1)))   }
+  else if( x <=  1e-3  &  x > 1e-6  ){ return(c(pre="m",x=format(x/1e-3,digit=1)))   }
+  else if( x <=  1e-6  &  x > 1e-9  ){ return(c(pre="mu",x=format(x/1e-6,digit=1)))  }
+  else if( x <=  1e-9  &  x > 1e-12 ){ return(c(pre="n",x=format(x/1e-9,digit=1)))  }
+  else if( x <= 1e-12  &  x > 1e-15 ){ return(c(pre="p",x=format(x/1e-12,digit=1)))  }
+  else if( x <= 1e-15  &  x > 1e-18 ){ return(c(pre="f",x=format(x/1e-15,digit=1)))  }
+  else if( x <= 1e-18  &  x > 1e-21 ){ return(c(pre="a",x=format(x/1e-18,digit=1)))  }
+  else if( x <= 1e-21  &  x > 1e-24 ){ return(c(pre="z",x=format(x/1e-21,digit=1)))  }
+  else if( x <= 1e-24 ){ return(c(pre="y",x=format(x/1e-24,digit=1)))  }
+}
+
+# Amino-acids charge (SEQ must be a character vector)
+is.pos <- function(SEQ){ SEQ == "K" | SEQ == "R" }
+is.neg <- function(SEQ){ SEQ == "D" | SEQ == "E" }
+POS <- function(SEQ){ sum(is.pos(SEQ)) }
+NEG <- function(SEQ){ sum(is.neg(SEQ)) }
+fpos <- function(SEQ){ mean(is.pos(SEQ)) }
+fneg <- function(SEQ){ mean(is.neg(SEQ)) }
+CHARGED <- function(SEQ){ return( POS(SEQ) + NEG(SEQ) ) }
+FCR  <- function(SEQ){ return( fpos(SEQ) + fneg(SEQ)  ) }
+NCPR <- function(SEQ){ return( abs( fpos(SEQ) - fneg(SEQ) )  ) }
+CHARGE.ASYM <- function(p,n){ return( (p-n)**2 / (p+n) ) }
+
+# Amino-acid sequence
+seq2fasta <- function(seq, maxline=70, as.string=F){
+  single = unlist(strsplit(as.character(seq),split=''))
+  string = paste(single,collapse='')
+  L = nchar(string)
+  fasta.nrows = (L %/% maxline)
+
+  fasta.seq = c()
+  for(i in 0:(fasta.nrows)){
+    start=(i*maxline) +1
+    fasta.seq= c( fasta.seq, substr(string,start,start+70) )
+  }
+  if(as.string){ return( paste0(fasta.seq,collapse='') ) }
+  return( paste0(fasta.seq,collapse='\n') )
+}
+
+# Colors -----------------------------------------------------------------------
+SetTextContrastColor <- function(color){
+  ifelse( mean(col2rgb(color)) > 127, "black", "white")
+}
+
+# perceived brightness
+#brightness = function(r, g, b){  return (r * 299 + g * 587 + b * 114) / 1000 }
+#brighterThan = function(r, g, b, x) { return (brightness(r,g,b) > x); }
+
+# Desaturate colors by specified proportion
+desat <- function(cols, sat=0.5) {
+  X <- diag(c(1, sat, 1)) %*% rgb2hsv(col2rgb(cols))
+  hsv(X[1,], X[2,], X[3,])
+}
+
+gg_color_hue <- function(n) {
+  hues = seq(15, 375, length = n + 1)
+  hcl(h = hues, l = 65, c = 100)[1:n]
+}
+
+
+# Environment ------------------------------------------------------------------
+script.to.env = function(src,nm){
+  # load script object into separate environment
+  env = attach(what = NULL,name = nm);
+  sys.source(file=src,env)
+}
+
+detachAllPackages <- function() {
+  basic.packages <- paste0("package:",c("stats","graphics","grDevices","utils","datasets","methods","base"))
+  package.list <- search()[ifelse(unlist(gregexpr("package:",search()))==1,TRUE,FALSE)]
+  package.list <- setdiff(package.list,basic.packages)
+  if (length(package.list)>0)  for (package in package.list) detach(package, character.only=TRUE)
+}
+
+showMemoryUse <- function(sort="size", decreasing=FALSE, limit) {
+  objectList <- ls(parent.frame())
+  oneKB <- 1024
+  oneMB <- 1048576
+  oneGB <- 1073741824
+  memoryUse <- sapply(objectList, function(x) as.numeric(object.size(eval(parse(text=x)))))
+  memListing <- sapply(memoryUse, function(size) {
+    if (size >= oneGB) return(paste(round(size/oneGB,2), "GB"))
+    else if (size >= oneMB) return(paste(round(size/oneMB,2), "MB"))
+    else if (size >= oneKB) return(paste(round(size/oneKB,2), "kB"))
+    else return(paste(size, "bytes"))
+  })
+  memListing <- data.frame(objectName=names(memListing),memorySize=memListing,row.names=NULL)
+  if (sort=="alphabetical") memListing <- memListing[order(memListing$objectName,decreasing=decreasing),]
+  else memListing <- memListing[order(memoryUse,decreasing=decreasing),] #will run if sort not specified or "size"
+  if(!missing(limit)) memListing <- memListing[1:limit,]
+  print(memListing, row.names=FALSE)
+  return(invisible(memListing))
+}
+
+.ls.objects <- function (pos = 1, pattern, order.by, decreasing=FALSE, head=FALSE, n=5) {
+  napply <- function(names, fn) sapply(names, function(x) fn(get(x, pos = pos)))
+  names <- ls(pos = pos, pattern = pattern)
+  obj.class <- napply(names, function(x) as.character(class(x))[1])
+  obj.mode <- napply(names, mode)
+  obj.type <- ifelse(is.na(obj.class), obj.mode, obj.class)
+  obj.size <- napply(names, object.size)
+  obj.dim <- t(napply(names, function(x) as.numeric(dim(x))[1:2]))
+  vec <- is.na(obj.dim)[, 1] & (obj.type != "function")
+  obj.dim[vec, 1] <- napply(names, length)[vec]
+  out <- data.frame(obj.type, obj.size, obj.dim)
+  names(out) <- c("Type", "Size", "Rows", "Columns")
+  if (!missing(order.by))
+    out <- out[order(out[[order.by]], decreasing=decreasing), ]
+  if (head)
+    out <- head(out, n)
+  out
+}
+
+# shorthands -------------------------------------------------------------------
+
+lsos <- function(..., n=10) { # checks top10 memory consumption from R objects
+  .ls.objects(..., order.by="Size", decreasing=TRUE, head=TRUE, n=n)
+}
+
+# precomputed data -------------------------------------------------------------
+sticky = c("A"= 0.0062, "C"= 1.0372, "D"=-0.7485, "E"=-0.7893, "F"= 1.2727,
+           "G"=-0.1771, "H"= 0.1204, "I"= 1.1109, "K"=-1.1806, "L"= 0.9138,
+           "M"= 1.0124, "N"=-0.2693, "P"=-0.1799, "Q"=-0.4114, "R"=-0.0876,
+           "S"= 0.1376, "T"= 0.1031, "V"= 0.7599, "W"= 0.7925, "Y"= 0.8806)
