2fe46b623d40dd12734675f5b9084d994a9bdfd5
benjamin-elusers <benjamin.dubreuil@weizmann.ac.il>
2021-12-12 11:45:27 +0200

    Adds two new analysis: - Alphafold disorder comparison - Deciphering evoltionary rate

---
+++ analysis/alphafold-3dcomplex.R
@@ -0,0 +1,11 @@
+# Connect 3DComplex V0 (local)
+hostname='els141g'
+user='elevy'
+pass='mysql!'
+
+
+# Correlation frac Alphafold vs IUP20 or D2P2 -> r=0.6
+# Alphafold vs D2P2 -> check outliers on scatterplot
+# Distribution abundance in PDB
+
+# CHECK DISORDER PREDICTIONS (% COVERAGE) VS ALPHAFOLD pDT
---
+++ analysis/decipher_evorate.R
@@ -0,0 +1,7 @@
+source(here::here("src","__setup_yeastomics__.r"))
+
+# Normalize mean evolutionary rate
+# Filter variables (abs cor MPC<0.4)
+# Make M0 and M3
+# Refine variables selections
+
