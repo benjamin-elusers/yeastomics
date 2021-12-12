23f0aa97b2935f49478e632f61339c1a30d5b043
benjamin-elusers <benjamin.dubreuil@weizmann.ac.il>
2021-12-12 14:10:50 +0200

    edit setup scripts

--- src/__setup_yeastomics__.r
+++ src/__setup_yeastomics__.r
@@ -24,7 +24,6 @@ violet= RColorBrewer::brewer.pal(name = 'Paired',n=12)[9:10]
 purple = colorRampPalette(colors = violet)(5)
 
 # Visualization themes
-options(dplyr.summarise.inform = FALSE, dplyr.width=Inf)
 theme_set(theme_bw(base_size=16,base_line_size = 0.5) +theme(plot.background = element_blank(),aspect.ratio=1) )
 
 mytheme = theme(plot.background = element_blank(),
@@ -34,14 +33,6 @@ mytheme = theme(plot.background = element_blank(),
                 axis.text=element_text(color='black',size=10,family="Helvetica"),
                 axis.line = element_line(color='black',size=0.5),
                 text=element_text(family='Helvetica'))
+# Options
+options(dplyr.summarise.inform = FALSE, dplyr.width=Inf)
 
-# source("https://raw.githubusercontent.com/clauswilke/dviz.supp/master/R/dviz.supp.R")
-# source("https://raw.githubusercontent.com/clauswilke/dviz.supp/master/R/themes.R")
-# empty_theme <- theme_dviz_open(12, rel_small = 1, rel_large = 1) +
-#   theme(
-#     axis.line = element_blank(),
-#     axis.text = element_blank(),
-#     axis.ticks = element_blank(),
-#     axis.ticks.length = grid::unit(0, "pt")
-#   )
-# theme_set( theme_modern(base_size = 16, axis.text.angle = 45,legend.position = 'none') + theme(aspect.ratio=1) )
