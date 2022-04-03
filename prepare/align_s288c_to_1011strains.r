0c1c6cfdb78abddb9140537283b0faa7f99c0bfd
benjamin-elusers <benjamin.dubreuil@weizmann.ac.il>
2022-04-03 10:59:34 +0300

    Remove last stop codon from all sequences (keep same length for all sequences)
    Write aligned sequences in a folder

--- prepare/align_s288c_to_1011strains.r
+++ prepare/align_s288c_to_1011strains.r
@@ -54,12 +54,11 @@ align_s288c_strains = function(orf){
   return(df_strain)
 }
 
-
 #### MAIN ####
 S288C = load.sgd.proteome(withORF=T,rm.stop=F) # Reference SGD protein sequences
 CDS = load.sgd.CDS()
 yk11_strains = load.peter2018.data(1) %>% pull(standardized_name) %>% sort
-snp_count_per_orf=read_rds(here("data",'YK11-ORF-VAR.rds'))
+snp_count_per_orf=read_rds(here("data",'YK11-ORF-VAR_AA.rds'))
 
 yk11_cds = "/media/elusers/users/benjamin/A-PROJECTS/01_PhD/02-abundance-evolution/strains1011/data/transfer_1638744_files_c25fb55c/CDS_withAmbiguityRes/"
 cds = read.sequences(seqfiles = list.files(yk11_cds,pattern='.fasta',full.names = T),type='DNA',strip.fname = T)
@@ -73,6 +72,10 @@ orfs = intersect(names(S288C),yk11_orfs)
 ## Add S288C and gapped sequence for missing strains ##
 yk11 = sapply(orfs, function(x){ add_missing_strains( BS=get_s288c_strain(x),all_strains=yk11_strains) })
 
+# Remove the stop codon at the end of the sequences (keep the same length for all sequences)
+yk11_nostop = sapply(yk11,stripR)
+yk11=yk11_nostop
+
 ## Find the 1000 most variable orfs (e.g. containing the most genetic variations) ##
 most_var = snp_count_per_orf %>% ungroup() %>%
   dplyr::mutate( f_var = n_var/len,
@@ -81,6 +84,7 @@ most_var = snp_count_per_orf %>% ungroup() %>%
   arrange(rk_var) %>% dplyr::filter(rk_var<=1000) %>%
   mutate(total_nvar = sum(n_var), total_len = sum(len),total_fvar =100*total_nvar/total_len)
 orf_var = sort(unique(most_var$id))
+orf_var = names(yk11)
 
 ## Find orfs not identical to s288c ##
 LL  = sapply(yk11,function(x){ unique(nchar(x))})
@@ -115,25 +119,32 @@ for( orf in names(realigned) ){
 
 ## Output the fasta files with s288c and all the 1011 strains
 # Gapped sequence == missing ORF in strains
-final_dir = "/data/benjamin/NonSpecific_Interaction/Data/Evolution/eggNOG/1011G/top1000_strain_with_s288c/"
+#final_dir = "/data/benjamin/NonSpecific_Interaction/Data/Evolution/eggNOG/1011G/top1000_strain_with_s288c/"
+final_dir = "/data/benjamin/NonSpecific_Interaction/Data/Evolution/eggNOG/1011G/fasta_strain_with_s288c/"
 dir.create(final_dir)
-for( o in orf_var ){
+for( o in names(yk11) ){
+
+  strains = get.strain_orf(names(yk11[[o]]),what = 'strain')
+  strains[is.na(strains)] = 'S288C'
+
   if( o %in% names(realigned) ){
     # WEIRD ORF (NOT UNIQUE LENGTH)
     if( check_realigned[o] ){
       # REALIGNED SEEMS FINE (SAME LENGTH, NOT TOO MANY (LONG) INDELS)
       tofasta = sprintf("%s/%s_realigned.fasta",final_dir,o)
       final_ali = add_missing_strains(as(realigned[[o]],'AAStringSet'),all_strains=yk11_strains)
+      #print(unique(widths(final_ali)))
     }else{
       print(sprintf("AVOIDING WEIRD ALIGNED ORF %s",o))
     }
   }else{
     # NORMAL ORFS
-    tofasta = sprintf("%s/%s.fasta",final_dir,o)
-    final_ali = stripR(yk11[[o]])
+    tofasta = sprintf("%s/%s_strains_only.fasta",final_dir,o)
+    final_ali = yk11[[o]]
   }
-  # final_ali = as(yk11[[o]], "AAStringSet")
-  # realigned = c(realigned, as(yk11[[o]], "AAStringSet"))
+
+  names(final_ali) = strains
+
   Biostrings::writeXStringSet(final_ali,filepath=tofasta,format = 'fasta')
 }
 
