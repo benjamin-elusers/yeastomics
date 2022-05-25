c14743bf4d8519680872f25398d531f2ea8302d8
benjamin-elusers <benjamin.dubreuil@weizmann.ac.il>
2022-05-25 20:47:03 +0300

    count_pair_ali(): if REF_NAME is null replace it with the ID

--- src/function_alignment.r
+++ src/function_alignment.r
@@ -121,6 +121,7 @@ count_pair_ali = function(ali){
 msa2df = function(MSA_SEQ,REF_NAME,ID){
 
   if(is(MSA_SEQ,"AAStringSet")){ MSA_SEQ = AAMultipleAlignment(MSA_SEQ)}
+  if(is.null(REF_NAME)){ REF_NAME=ID }
   L = nchar(MSA_SEQ)
   N = length(unmasked(MSA_SEQ))
   IREF = which(rownames(MSA_SEQ) %in% REF_NAME)
