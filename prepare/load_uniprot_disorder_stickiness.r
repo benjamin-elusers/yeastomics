cd2f764e66af6339b8a6627eb66496563d99dde6
benjamin-elusers <benjamin.dubreuil@weizmann.ac.il>
2022-06-26 16:10:50 +0300

    parallel to retrieve sequence and compute stickiness scores

--- prepare/load_uniprot_disorder_stickiness.r
+++ prepare/load_uniprot_disorder_stickiness.r
@@ -3,69 +3,58 @@ library(tidyverse)
 library(here)
 library(Biostrings)
 
-fetch.mobidb = function(ids,to.df=T){
-  # Check mobidb data description at the following url:
-  # https://mobidb.bio.unipd.it/about/mobidb
-  library(pbmcapply)
-  library(parallel)
-  library(tictoc)
-  cpus=detectCores()-2
-  tic('get disorder from MobiDB')
-  message(sprintf('retrieve disorder predictions from MobiDB for %s identifiers (uniprot) ...',n_distinct(ids)))
-  mobidb = pbmcapply::pbmclapply(ids, get.mobidb.id,mc.cores=cpus)
-  names(mobidb) = ids
-  toc()
-
-  if(to.df){
-    return( purrr::compact(mobidb) %>% bind_rows() %>% )
-  }
-
-  return(mobidb)
-}
-
-load.mobidb = function(taxon){
-  # Check mobidb data description at the following url:
-  # https://mobidb.bio.unipd.it/about/mobidb
-  tic('get mobidb for taxon from url....')
-  mobidb = readr::read_delim(sprintf("https://mobidb.bio.unipd.it/api/download?ncbi_taxon_id=%s&format=tsv",taxon)) %>%
-           separate(col=feature, sep='-',into=c('evidence','feature','source')) %>%
-           separate_rows('start..end',sep=',') %>%
-           separate('start..end',into = c('S','E'),convert = T) %>%
-           mutate( feature_len = E-S+1 )
-
-  toc()
-  return(mobidb)
-}
 
 
-# 1. Find the taxon id matching your keywords (case-insensitive)
+# 1. Find the taxon id matching your keywords (case-insensitive) ===============
 find.uniprot_refprot(c('human','homo','sapiens','eukaryota'))
 # For example, to find the bacteria E. coli
 ecoli = find.uniprot_refprot(c('bacteria','Escherichia','coli','K12'))
 print(ecoli$tax_id) # Taxon id = 83333
 
-# 2. Get the reference proteome sequences for the selected taxon
+# 2. Get the reference proteome sequences for the selected taxon ===============
 ec_aa = get.uniprot.proteome(taxid = 83333, DNA = F)
 
-# 3. Get the reference uniprot accession for the selected taxon
-ec_uniref = names(ecoli_prot)
+# 3. Get the reference uniprot accession for the selected taxon ================
+ec_uniref = names(ec_aa)
 
-# 4. Get all disorder predictions (from taxon id or from list of ids)
-# using a vector of uniprot ids
-EC_MOBIDB = fetch.mobidb(ec_uniref) # Took 130 sec. for 4402 identifiers from e. coli
-# using a taxon id
-ec_mobidb = load.mobidb(83333) # much faster and return potentially more results
+# 4. Get all disorder predictions (from taxon id or from list of ids) ==========
+# ------> Using a vector of uniprot ids <------
+# EC_MOBIDB = fetch.mobidb(ec_uniref) # Took 130 sec. for 4402 identifiers from e. coli
+# n_distinct(EC_MOBIDB$acc)
 
-n_distinct(ec_diso$acc)
-# 5. Get the sequence for disorder stretches and compute amino-acid scores
+# Using a taxon id
+ec_mobidb = load.mobidb(83333) %>% # Took 20 sec. for the protein identifiers of the taxon
+            dplyr::filter(acc %in% ec_uniref) # Only use uniprot reference proteome
+n_distinct(ec_mobidb$acc)
+
+# 5. Get the sequence for disorder stretches and compute amino-acid scores =====
+ec_sticky = ec_mobidb %>%
+            rowwise()
 
 
-ec_sticky =
-            rowwise() %>%
+library(furrr)
+library(progressr)
+library(dplyr)
 
-                    feature_aa  = as.character(subseq(ec_aa[[acc]],start=as.integer(S), end=as.integer(E))) )
+tic('retrieve regions sequence...')
+plan(multisession, workers = 14)
+with_progress({
+  p <- progressor(steps = nrow(ec_sticky), message = 'extract sequence from region start-end positions...')
 
-head(ec_sticky)
-table( ec_sticky$feature_len )
+  diso_seq <- furrr::future_map(1:nrow(ec_sticky), ~{
+    p()
+    subseq(x=ec_aa[[ec_sticky$acc[.x]]], start=as.integer(ec_sticky$S[.x]), end=as.integer(ec_sticky$E[.x]))
+  })
+})
+toc()
 
-Biostrings::extract_character_from_XString_by_ranges(ec_aa[[1]],start = 10L,width=10L)
+tic('compute aa score...')
+with_progress({
+  p <- progressor(steps = length(diso_seq), message = 'calculate amino acid scores for sequences...')
+
+  diso_score <- furrr::future_map(seq_along(diso_seq), ~{
+    p()
+    get_aa_score(diso_seq[[.x]])
+  })
+})
+
