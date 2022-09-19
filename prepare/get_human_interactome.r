f02c90f9410ed39dac1fc3495d2fdb632d12ab92
benjamin-elusers <benjamin.dubreuil@weizmann.ac.il>
2022-09-19 15:52:53 +0300

    Attempt to quantify the true number of interaction partners for a protein (small ligands, Protein, DNA...)
    Calculating the number of repeated k-mers

--- prepare/get_human_interactome.r
+++ prepare/get_human_interactome.r
@@ -66,3 +66,108 @@ mean(intact.cent$cent_deg) # Average interactors (degree)
 median(intact.cent$cent_deg) # Median interactors (degree)
 quantile(intact.cent$cent_deg,seq(0,1,len=21)) # quantile degree
 
+
+
+### homo-oligomers
+library(RMySQL)
+#To get the refset:
+mydb = dbConnect(MySQL(), user='elevy', password='Mysql1!', dbname='3dcomplexV0', host='els141g.weizmann.ac.il')
+full_refset = dbSendQuery(mydb, paste0("select * from proteome where refset = '1'"))
+data_full_refset = fetch(full_refset, n=-1)
+#Code above will give you all the refset proteins from the four model organisms.
+count_uniref= data_full_refset %>% ungroup() %>% group_by(org) %>% summarize(nprot=n_distinct(code_sub))
+#The column with symmetry info is sym_grom_best.
+# please take note that if you want to have all the homomers (and not only the refset) you need to do the following query:
+prot_homo = dbSendQuery(mydb, paste0("select * from proteome where refset='1' or
+                                    (PDBsym_homo is not null and PDBsym_homo != 'NPS')"))
+data_prot_homo = fetch(prot_homo, n=-1) %>% as_tibble() %>% type_convert()
+
+
+homomers = data_prot_homo %>%
+           dplyr::select(OX,OS,org,org_code,
+                         uniprot = code_sub, gene, GN, AF=code, pdb=PDBcode,
+                         symAF, sym_geom_best, qs_nsub, L=length_full, dom_arch_pfam, ab,abQ,abMax) %>%
+          mutate( homomers = factor(qs_nsub,ordered = T, labels = unique(na.omit(map_chr(sort(data_prot_homo$qs_nsub),to_oligomer))) ),
+                  multimer = fct_other(homomers,keep=c('monomer','dimer'),other_level = 'multimer'),
+                  multi = fct_other(sym_geom_best,keep=c('NPS','C2'),other_level = 'multimer') )
+
+count_homomers = homomers %>%
+                  group_by(org) %>%
+                  count(multi) %>%
+                  add_tally(n,name = 'total_org') %>%
+                  ungroup() %>%
+                  mutate( fr = 100*n/total_org) %>%
+                  mutate( fr = 100*n/total_org)
+
+
+ggplot(count_homomers, aes(x=multi,y=fr,color=org,fill=org)) +
+  geom_col(position = position_dodge(0.8),width = 0.5) +
+  geom_text(aes(y=0,label=sprintf("%s",n)),color='white',angle=90, hjust=-0.1,position=position_dodge(width = 0.8),size=3.5) +
+  scale_fill_metro() + scale_color_metro()
+
+tab_count_homomers = homomers %>%
+  janitor::tabyl(multi,org) %>%
+  janitor::adorn_percentages('col') %>%
+  janitor::adorn_pct_formatting(digits = 2)
+
+#  janitor::adorn_totals()
+
+#This is because some known homomers are not detected with Alphafold
+
+#You can also access a code that generate the distribution of symmetries per organism + the fraction of homomers in the proteomes of the four organisms there:
+#  /media/elusers/users/hugo/15_alphafold/06_proteome_analysis/plot_symmetries_refset.R
+
+
+
+##
+#/home/benjamin/Desktop/GitHub/yeastomics/data/cavidb
+cavspace = here::here('data','cavityspace','AF-all_cavities.idx') %>% read_delim('\t') %>% janitor::clean_names()
+bindingdb = here::here('data','bindingdb','BindingDB_All.tsv') %>% read_delim('\t') %>% janitor::clean_names()
+dnaprodb =  here::here('data','dnaprodb','collection.txt') %>% rjson::fromJSON(file=.) %>% janitor::clean_names()
+
+dim(cavspace)
+head(cavspace)
+
+head(bindingdb)
+
+enumerate_seq = function(BS,k=5){
+  w=nchar(BS)
+  words = lapply(1:(w-k), function(p0){ subseq(BS, start = p0, width = k) %>% as.character }) %>% unlist
+  return(words)
+}
+
+enumerate = function(proteome, K=3:12){
+  library(pbmcapply)
+  N = length(proteome)
+  words_by_size=list()
+  for( k in K ){
+    message(sprintf("Enumerate %s-mer in %s sequences...",k,N))
+    words_by_size[[paste0("k",k)]] = pbmclapply(sc_aa, enumerate_seq,k=k,mc.cores=14)
+  }
+  return(words_by_size)
+}
+
+sc_aa=load.sgd.proteome()
+sc_kmer = enumerate(sc_aa, K=3:15)
+
+k3 = sc_kmer$k3 %>% unlist %>% table
+k4 = sc_kmer$k4 %>% unlist %>% table
+length(k4) / min(20^unique(nchar(sc_kmer$k4%>%unlist)), length(sc_kmer$k4 %>% unlist))
+
+nmer  = sapply(sc_kmer, function(x){ unlist(x) %>% length })
+kspace = sapply(sc_kmer, function(x){ mers = unlist(x); 20^(unique(nchar(mers))) })
+
+get_repeats = function(words_by_seq,nmin=3){ map_dfrsapply(words_by_seq, function(w){ table(w) %>% as_tibble }) }
+
+sc_words = map_dfr(sc_kmer,stack) %>%
+            set_names('word','orf') %>%
+            filter( !str_detect(word,"\\*") ) %>%
+            mutate(k=nchar(word)) %>%
+            group_by(word,orf,k) %>%
+            summarize( nr = n()) %>%
+            group_by(word,k) %>%
+            mutate( NR = sum(nr), NP = n() )
+
+dim(sc_words)
+sc_repeats = sc_words %>% filter(nr>1 & NP > 100)
+head(sc_repeats)
