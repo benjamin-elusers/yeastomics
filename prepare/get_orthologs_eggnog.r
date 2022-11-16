1935a0eeb373b23a0446288f983ab10c47c1eee4
benjamin-elusers <benjamin.dubreuil@weizmann.ac.il>
2022-11-16 11:40:29 +0200

    Retrieve fungi and mammals data from eggnog

---
+++ prepare/get_orthologs_eggnog.r
@@ -0,0 +1,29 @@
+source(here::here("src","__setup_yeastomics__.r"))
+library(tidyverse)
+library(here)
+
+# Fungi = 4751
+
+fuNOG = get_eggnog_node(node = 4751)
+fu_species = get_eggnog_species(node = 4751)
+
+fu_yeast = eggnog_annotations_species(node = 4751, species = c(4932,4896))
+fu_taxons = count_taxons_eggnog_node(4751)
+
+fu_fasta = load_eggnog_fasta(fuNOG$OG)
+#fu_ali = get_eggnog_alignment(node=4751, use_trimmed = F)
+
+
+#eggnog_fasta = Biostrings::readAAStringSet("http://eggnog5.embl.de/download/latest/e5.proteomes.faa")
+
+# Mammalia = 40674
+
+maNOG = get_eggnog_node(40674)
+ma_species = get_eggnog_species(node = 40674)
+
+ma_human = eggnog_annotations_species(node = 40674, species = c(9606))
+ma_taxons = count_taxons_eggnog_node(40674)
+
+#ma_ali = get_eggnog_alignment(node=40674, use_trimmed = F)
+
+ma_fasta = load_eggnog_fasta(maNOG$OG)
